8bbdf7bc3d63440b39cae86a590df6fa1437e9dc
Kiyarash Khoshmood <kkh.blackcenter@yahoo.com>
2024-07-01 12:43:43 +0300

    add2

---
+++ resources/views/profile.blade.php
@@ -0,0 +1,13 @@
+<h1>
+    this is profile page
+
+</h1>
+
+<p>
+    followers 0
+</p>
+<p>
+    following 0
+</p>
+
+<button>Follow</button>
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\FollowController;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,3 +17,5 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/profile', [FollowController::class, 'index'])->name('profile.index');
